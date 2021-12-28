e7fc7f41de8296be0602eb9f4c98ced73e422c26
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-12-27 19:31:19 -0500

    add react-routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,23 @@
 import { Component } from "react";
+import { Link } from "react-router-dom";
 import { User } from "../model/Model";
 
 export class Navbar extends Component<{
-  user: User;
+  user: User | undefined;
 }> {
   render() {
-    return <div>Welcome to the Home Page</div>;
+    let loginLogout: any;
+    if (this.props.user) {
+      loginLogout = <Link to="/logout">{this.props.user.userName}</Link>;
+    } else {
+      loginLogout = <Link to="/login">Login</Link>;
+    }
+    return (
+      <div className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        {loginLogout}
+      </div>
+    );
   }
 }
