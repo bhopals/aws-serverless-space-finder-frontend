78e5ce0763fe46394e4f47781a48f1db702a035b
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-12-28 16:47:49 -0500

    add routing and package upgrade

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { User } from "../model/Model";
 import { AuthService } from "../services/AuthService";
 import { Login } from "./Login";
+import { Router, Route, Switch } from "react-router-dom";
+import history from "../utils/history";
+import { Navbar } from "./Navbar";
+import { Home } from "./Home";
+import { Profile } from "./Profile";
+
 interface AppState {
   user: User | undefined;
 }
@@ -10,6 +16,9 @@ export class App extends React.Component<{}, AppState> {
 
   constructor(props: any) {
     super(props);
+    this.state = {
+      user: undefined,
+    };
     this.setUser = this.setUser.bind(this);
   }
   private setUser(user: User) {
@@ -19,10 +28,20 @@ export class App extends React.Component<{}, AppState> {
 
   render() {
     return (
-      <>
-        <Login authService={this.authService} setUser={this.setUser} />
-        <div>App Works!!</div>
-      </>
+      <div className="wrapper">
+        <Router history={history}>
+          <div>
+            <Navbar user={this.state.user} />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login">
+                <Login authService={this.authService} setUser={this.setUser} />
+              </Route>
+              <Route exact path="/profile" component={Profile} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
     );
   }
 }
