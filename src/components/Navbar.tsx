f42673c0a5d7aa6486205d77c415bbb1c2893e5a
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-12-29 08:46:15 -0500

    add css styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,9 +8,17 @@ export class Navbar extends Component<{
   render() {
     let loginLogout: any;
     if (this.props.user) {
-      loginLogout = <Link to="/logout">{this.props.user.userName}</Link>;
+      loginLogout = (
+        <Link to="/logout" style={{ float: "right" }}>
+          {this.props.user.userName}
+        </Link>
+      );
     } else {
-      loginLogout = <Link to="/login">Login</Link>;
+      loginLogout = (
+        <Link to="/login" style={{ float: "right" }}>
+          Login
+        </Link>
+      );
     }
     return (
       <div className="navbar">
