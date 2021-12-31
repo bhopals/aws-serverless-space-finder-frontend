60c97d90b96965e9d4a97f5b5460838a5ba3b1f7
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-12-30 19:12:14 -0500

    POST Space - store to Dynamo DB

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -1,9 +1,50 @@
 import { ICreateSpaceState } from "../components/spaces/CreateSpace";
 import { Space } from "../model/Model";
+import { S3, config } from "aws-sdk";
+import { config as appConfig } from "./config";
+import { resolveModuleName } from "typescript";
 
+config.update({
+  region: appConfig.REGION,
+});
 export class DataService {
+  // private s3Client = new S3({
+  //   region: appConfig.REGION,
+  // });
+
   public async createSpace(iCreateSpace: ICreateSpaceState) {
-    return "123";
+    if (iCreateSpace.photo) {
+      const photoUrl = await this.uploadPublicFile(
+        iCreateSpace.photo,
+        appConfig.SPACES_PHOTOS_BUCKET
+      );
+      iCreateSpace.photoURL = photoUrl;
+      iCreateSpace.photo = undefined;
+    }
+    const requestUrl = appConfig.api.spacesUrl;
+    const requestOptions: RequestInit = {
+      method: "POST",
+      body: JSON.stringify(iCreateSpace),
+    };
+
+    const result = await fetch(requestUrl, requestOptions);
+    const resultJSON = await result.json();
+    return JSON.stringify(resultJSON.id);
+  }
+
+  public async uploadPublicFile(file: File, bucket: string) {
+    const fileName = file.name;
+    const uploadResult = await new S3({
+      region: appConfig.REGION,
+    })
+      .upload({
+        Bucket: bucket,
+        Key: fileName,
+        Body: file,
+        ACL: "public-read",
+      })
+      .promise();
+    return uploadResult.Location;
   }
 
   public async getSpaces(): Promise<Space[]> {
--- src/services/config.ts
+++ src/services/config.ts
@@ -1,3 +1,4 @@
+const spaceUrl = "http://vlbrdes3434.execute-api.eu-west-1.amazonaws.com/prod/";
 export const config = {
   REGION: "us-east-1",
   USER_POOL_ID: "us-east-1_JWds32f42", // User Pool Details
@@ -5,4 +6,9 @@ export const config = {
   IDENTITY_POOL_ID: "us-east-1:9bserasdfasewfwefw", // IDentity Pool Details
   TEST_USER_NAME: "test_user", // User Pool Details
   TEST_USER_PASSWORD: "askesfme3wf", // User Pool Details
+  SPACES_PHOTOS_BUCKET: "spaces-photos-0238058255",
+  api: {
+    baseUrl: spaceUrl,
+    spacesUrl: `${spaceUrl}spaces`,
+  },
 };
